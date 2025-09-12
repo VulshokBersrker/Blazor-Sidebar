57a3adc4897308075c70833cade43cade143cf52
Tyler Kierstein <35084718+VulshokBersrker@users.noreply.github.com>
2025-09-12 18:16:55 -0400

    Forecast Section Started
    Forecast section has been started, and has limited functionality so far

--- projectOne/obj/Debug/net9.0/projectOne.AssemblyInfo.cs
+++ projectOne/obj/Debug/net9.0/projectOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4c1fb19009ed0d7c0d0b6a47f18e085bc2b249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b34e70cbde56d3e688c05a9c3d098893d423cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
